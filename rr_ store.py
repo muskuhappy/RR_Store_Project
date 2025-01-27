c62fedd15ac53764acdf2ec85db6195c82b05ce0
muskuhappy <happygyanchandani7@gmail.com>
2025-01-27 21:39:19 +0530

    Add files via upload

---
+++ rr_ store.py
@@ -0,0 +1,104 @@
+print("welcome at rr store","\n","we provides:","\n")
+print("\t \t \t","1.vegetables","\n")
+print("\t \t \t","2.groceries","\n")
+print("\t \t \t","3.dairy","\n")
+print("\t \t \t","4.bakery","\n")
+print("\t \t \t","5.fruits","\n")
+a=input("choose your categoery:")
+if a == '1':
+    print("in vegetables we have","\n")
+    print("\t \t \t","1.patato","\n")
+    print("\t \t \t","2.tomato","\n")
+    print("\t \t \t","3.okra","\n")
+    print("\t \t \t","4.egg plant","\n")
+    print("\t \t \t","5.spinach","\n")
+    b=input("choose your vegetable:")
+    if(b=='1'):
+      print("patato in your cart.")
+    elif(b=='2'):
+      print("tomato in your cart.")
+    elif(b=='3'):
+      print("okra in your cart.")
+    elif(b=='4'):
+      print("egg plant in your cart.")
+    else:
+      print("spinach in your cart.")
+      
+
+elif a == '2':
+    print("in groceries we have","\n")
+    print("\t \t \t","1.oats","\n")
+    print("\t \t \t","2.coffee","\n")
+    print("\t \t \t","3.tea","\n")
+    print("\t \t \t","4.oil","\n")
+    print("\t \t \t","5.suger","\n")
+    c=input("choose your groceries:")
+    if(c=='1'):
+      print("oats in your cart.")
+    elif(c=='2'):
+      print("coffee in your cart.")
+    elif(c=='3'):
+      print("tea in your cart.")
+    elif(c=='4'):
+      print("oil in your cart.")
+    else:
+      print("suger in your cart.")
+
+elif a == '3':
+    print("in dairy we have","\n")
+    print("\t \t \t","1.butter","\n")
+    print("\t \t \t","2.milk","\n")
+    print("\t \t \t","3.quard","\n")
+    print("\t \t \t","4.cream","\n")
+    print("\t \t \t","5.cheese","\n")
+    d=input("choose your dairy:")
+    if(d=='1'):
+      print("butter in your cart.")
+    elif(d=='2'):
+      print("milk in your cart.")
+    elif(d=='3'):
+      print("quard in your cart.")
+    elif(d=='4'):
+      print("cream in your cart.")
+    else:
+      print("cheese in your cart.")
+
+elif a == '4':
+    print("in bakery we have","\n")
+    print("\t \t \t","1.cakes","\n")
+    print("\t \t \t","2.cookies","\n")
+    print("\t \t \t","3.pizza","\n")
+    print("\t \t \t","4.biscuit","\n")
+    print("\t \t \t","5.bread rolls","\n")
+    e=input("choose your bakery:")
+    if(e=='1'):
+      print("cakes in your cart.")
+    elif(e=='2'):
+      print("cookies in your cart.")
+    elif(e=='3'):
+      print("pizza in your cart.")
+    elif(e=='4'):
+      print("biscuit in your cart.")
+    else:
+      print("bread rolls in your cart.")
+
+else:
+    print("in fruits we have","\n")
+    print("\t \t \t","1.apple","\n")
+    print("\t \t \t","2.banana","\n")
+    print("\t \t \t","3.lychee","\n")
+    print("\t \t \t","4.watermelon","\n")
+    print("\t \t \t","5.coconut","\n")
+    f=input("choose your fruits:")
+    if(f=='1'):
+      print("apple in your cart.")
+    elif(f=='2'):
+      print("banana in your cart.")
+    elif(f=='3'):
+      print("lychee in your cart.")
+    elif(f=='4'):
+      print("watermelon in your cart.")
+    else:
+      print("coconut in your cart.")
+
+print("\t \t \t \t ✌️😊")
